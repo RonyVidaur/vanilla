014c2b992756891ace0706b7cff5c8d94fe69044
RonyVidaur <rony.vidaur@gmail.com>
2018-07-14 15:11:29 -0600

    get/update/delete a single expense (account) support added

--- server/controllers/expenses.js
+++ server/controllers/expenses.js
@@ -1,5 +1,5 @@
 const Expense = require("../models").Expense;
-
+const Transaction = require("../models").Transaction;
 module.exports = {
   create(req, res) {
     return Expense.create({
@@ -9,8 +9,73 @@ module.exports = {
       .catch(error => res.status(400).send(error));
   },
   list(req, res) {
-    return Expense.all()
+    return Expense.findAll({
+      include: [
+        {
+          model: Transaction,
+          as: "transactions"
+        }
+      ]
+    })
       .then(expenses => res.status(200).send(expenses))
+      .catch(error => res.status(400).send(error));
+  },
+  retrieve(req, res) {
+    return Expense.findById(req.params.expenseId, {
+      include: [
+        {
+          model: Transaction,
+          as: "transactions"
+        }
+      ]
+    })
+      .then(expense => {
+        if (!expense) {
+          return res.status(404).send({
+            message: "Account not found"
+          });
+        }
+        return res.status(200).send(expense);
+      })
+      .catch(error => res.status(400).send(error));
+  },
+  update(req, res) {
+    return Expense.findById(req.params.expenseId, {
+      include: [
+        {
+          model: Transaction,
+          as: "transactions"
+        }
+      ]
+    })
+      .then(expense => {
+        if (!expense) {
+          return res.status(404).send({
+            message: "Account not found"
+          });
+        }
+        return expense
+          .update({
+            balance: req.body.balance || expense.balance
+          })
+          .then(() => res.status(200).send(expense))
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  },
+  destroy(req, res) {
+    return Expense.findById(req.params.expenseId)
+      .then(expense => {
+        if (!expense) {
+          return res.status(404).send({
+            message: "Account Not Found"
+          });
+        }
+        return expense
+          .destroy()
+          .then(() => res.status(204).send(expense))
+          .catch(error => res.status(400).send(error));
+      })
       .catch(error => res.status(400).send(error));
   }
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,13 +3,15 @@ const transactionsController = require("../controllers").transactions;
 module.exports = app => {
   app.get("/api", (req, res) =>
     res.status(200).send({
-      message: "Expenses API"
+      message: "Accounts API"
     })
   );
 
   /* expenses - accounts */
   app.post("/api/expenses", expensesController.create);
   app.get("/api/expenses", expensesController.list);
+  app.get("/api/expenses/:expenseId", expensesController.retrieve);
+  app.put("/api/expenses/:expenseId", expensesController.update);
   app.post(
     "/api/expenses/:expenseId/transactions",
     transactionsController.create
