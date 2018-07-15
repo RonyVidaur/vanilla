2a2dc41798d2b44553f60385ed564817bd202eed
RonyVidaur <rony.vidaur@gmail.com>
2018-07-14 20:16:14 -0600

    landing page update

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import { Button } from "react-bulma-components/full";
+import transaction from "../assets/img/transaction.svg";
 
 export default class Landing extends Component {
   render() {
     return (
-      <div>
-        <h1>The Easiest way to track your expenses.</h1>
+      <div className="landing">
+        <div className="left">
+          <h1>
+            <strong>Vanilla</strong> is the easiest way to manage your money
+          </h1>
+          <h2>Try it now by creating an account</h2>
+          <Button>Get Started</Button>
+        </div>
+        <div className="right">
+          <img src={transaction} alt="vanilla expense tracker" />
+        </div>
       </div>
     );
   }
