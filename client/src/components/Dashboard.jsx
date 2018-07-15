a44682c9fde9985d1bb02ff2b008d8d8e69342b5
RonyVidaur <rony.vidaur@gmail.com>
2018-07-14 22:57:10 -0600

    added balance to dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -11,10 +11,26 @@ export default class Dashboard extends Component {
     const { accounts, currentDate, currentUser } = this.state;
     return (
       <div className="dashboard">
-        <h1>
-          Hi <strong>{currentUser.firstName}</strong>,
-        </h1>
-        <h2>See where you spend your money</h2>
+        <div className="user-info">
+          <div className="dashboard-left">
+            <h1>
+              Hi <strong>{currentUser.firstName}</strong>,
+            </h1>
+            <h2>See where you spend your money</h2>
+          </div>
+          <div className="dashboard-right">
+            <h2>
+              Current Balance:{" "}
+              <span
+                className={
+                  accounts[0].balance > 0 ? "text green-text" : "text red-text"
+                }
+              >
+                ${accounts[0].balance}
+              </span>
+            </h2>
+          </div>
+        </div>
         <div className="info-holder">
           <CondensedInfo date={currentDate} />
         </div>
