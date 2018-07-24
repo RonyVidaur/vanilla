408a9ccee6f5e3665e1ed18fa76dd8acb70665ad
RonyVidaur <rony.vidaur@gmail.com>
2018-07-23 19:41:32 -0600

    option

---
+++ server/config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+  google: {
+    clientSecret: "vsna_4eY3_rvryUvvTrEux7j",
+    clientID:
+      "464520282331-pgnn9i6tctg3p8pj06bpbb7sd8cv7vd6.apps.googleusercontent.com"
+  },
+  session: {
+    cookieKey: "acklenavenueexprensetrackerapp"
+  }
+};
